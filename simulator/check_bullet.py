3f6a2673cb78300df35a6893c08772673a614378
Jad Nohra <jadnohra@users.noreply.github.com>
2017-08-16 21:50:14 +0200

    .

--- simulator/check_bullet.py
+++ simulator/check_bullet.py
@@ -3,7 +3,6 @@ p.connect(p.DIRECT)
 p.setGravity(0,0,-10)
 bodyId = p.createMultiBody(1.0, p.createCollisionShape(p.GEOM_SPHERE,radius=0.1), -1, [0,0,1], [0,0,0,1])
 p.stepSimulation()
-cubePos, cubeOrn = p.getBasePositionAndOrientation(bodyId)
-print(cubePos,cubeOrn)
+print p.getBasePositionAndOrientation(bodyId)[0][2]
 p.disconnect()
 
--- simulator/setup.sh
+++ simulator/setup.sh
@@ -1,2 +1,8 @@
 pip install pybullet
 python check_bullet.py
+
+if python ./check_bullet.py | grep -q '0.999826'; then
+  echo "Looks good"
+else
+  echo "Something is wrong"
+fi
